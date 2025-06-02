3fb728f9645b092dcae8fefa7b590cfca7b32441
codelockinfo <codelockinfo@gmail.com>
2025-06-02 09:45:24 +0530

    Remove extra scopes

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require 'vendor/autoload.php';
 
 $shop = $_GET['shop'] ?? '';
 $apiKey = "9a5adb07279a98af27fd73da38dabafe";
-$scopes = "read_all_orders,read_checkouts,read_customers,read_discounts,read_draft_orders,read_fulfillments,read_inventory,read_orders,read_products,write_checkouts,write_customers,write_draft_orders,write_fulfillments,write_orders,write_products,write_themes";
+$scopes = "read_checkouts,read_customers,read_discounts,read_draft_orders,read_fulfillments,read_inventory,read_orders,read_products,write_checkouts,write_customers,write_draft_orders,write_fulfillments,write_orders,write_products,write_themes";
 $redirectUri = "https://codelocksolutions.com/y-test-app-4/callback.php";
 
 $installUrl = "https://{$shop}/admin/oauth/authorize?client_id={$apiKey}&scope={$scopes}&redirect_uri={$redirectUri}";
